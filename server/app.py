04629485d695156180d503feb134a50e406188a3
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-07-04 12:40:18 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -20,7 +20,30 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude):
+        quakes.append(earthquake.to_dict())
+
+    body = {'count': len(quakes),
+            'quakes': quakes}
+    status = 200
+
+    return make_response(body, status)
 
 
 if __name__ == '__main__':
